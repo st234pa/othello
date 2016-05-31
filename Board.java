d686f18c55b9090e2ceb651f386d3031e4ed56f4
Stephanie <syoon1@stuy.edu>
2016-05-31 08:29:44 -0400

    3

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
-public class Board {
+public class Board implements Comparable<Board> {
 	private String[][] _board;
+	private int _eval;
 	public Board() {
 		_board = new String[8][8];
 		for (int i = 0; i < 8; i++) {
@@ -10,6 +11,10 @@ public class Board {
 		_board[4][3] = "x";
 		_board[4][4] = "o";
 		_board[3][3] = "o";
+		_eval = 0;
+	}
+	public String get(int i, int j) {
+		return _board[i][j];
 	}
 	public String toString() {
 		String ans = "  0 1 2 3 4 5 6 7 ";
@@ -22,5 +27,13 @@ public class Board {
 			ans += "\n";
 		}
 		return ans;
+	}
+	public int eval() {
+		return _eval;
+	}
+	public int compareTo(Board other) {
+		if (this.eval() > other.eval()) return 1;
+		else if (this.eval() == other.eval()) return 0;
+		else return -1;
 	}
 }
--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.PriorityQueue;
 public class Game {
 	private Board _board;
 	private int _consecPasses, _n, _tiles;
@@ -10,12 +11,16 @@ public class Game {
 	}
 	public void run() {
 		while (_tiles < 64 && _consecPasses < 2) {
+			System.out.print("\033[H\033[2J");
+			System.out.flush();
+			System.out.println(_board);
 			playerTurn();
+			if (_tiles < 64) 
+				computerTurn();
 		}
+		showResults();
 	}
 	public void playerTurn() {
-		System.out.print("\033[H\033[2J");
-		System.out.flush();
 		System.out.println("Player's turn.");
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Row: ");
@@ -27,10 +32,41 @@ public class Game {
 		}
 		else _consecPasses++;
 	}
+	public void computerTurn() {
+		System.out.println("Computer's turn.");
+		_board = bestMove(possibleMoves(_board));
+		System.out.println(_board);
+	}
 	public static boolean validMove(Board b, int r, int c) {
-
+		return true;
+	}
+	public static boolean goodFlip(Board b, int r, int c, int ud, int lr) {
+		return true;
+	}
+	public static Board flip(Board b, int r, int c,  int ud, int lr) {
+		return null;
 	}
 	public static Board makeMove(Board b, int r, int c) {
-		
+		return null;
+	}
+	public static PriorityQueue<Board> possibleMoves(Board b) {
+		return null;
+	}
+	public static Board bestMove(PriorityQueue<Board> moves) {
+		return moves.peek();
+	}
+	public void showResults() {
+		System.out.println("Game over.");
+		int player = 0;
+		int computer = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (_board.get(i,j).equals("x")) player++;
+				else if (_board.get(i,j).equals("o")) computer++;
+			}
+		}
+		if (player > computer) System.out.println("You win!");
+		else if (player < computer) System.out.println("You lose. :(");
+		else System.out.println("Draw.");
 	}
 }
