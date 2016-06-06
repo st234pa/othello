74730cb56640579035fbadacc2c63887072d8aa7
Stephanie <syoon1@stuy.edu>
2016-06-06 02:02:22 -0400

    asdf

--- Game.java
+++ Game.java
@@ -62,7 +62,7 @@ public class Game {
 	public boolean goodFlip(Board b, int r, int c, int ud, int lr) {
 		r+=ud;
 		c+=lr;
-		while(r+ud >= 0 && r+i<8 && c+lr>= 0 && c+lr <8 && !(b.get(r,c).equals("-"))) {
+		while(r+ud >= 0 && r+ud<8 && c+lr>= 0 && c+lr <8 && !(b.get(r,c).equals("-"))) {
 			if (b.get(r,c).equals("x")) return true;
 			r+=ud;
 			c+=lr;
