437550124eaa2ac555f59899d8f5fe2399895dc6
Stephanie <syoon1@stuy.edu>
2016-06-06 02:01:01 -0400

    asdf

--- Game.java
+++ Game.java
@@ -72,7 +72,7 @@ public class Game {
 	public Board flip(Board b, int r, int c,  int ud, int lr) {
 		r+=ud;
 		c+=lr;
-		while(r+i >= 0 && r+i<8 && c+j>= 0 && c+j <8 && b.get(r,c).equals("o"))) {
+		while(r+i >= 0 && r+i<8 && c+j>= 0 && c+j <8 && b.get(r,c).equals("o")) {
 			b.set(r,c,"x");
 			r+=ud;
 			c+=lr;
