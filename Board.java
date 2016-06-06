c503a079ba803eaa7ea9c3c57d5ea64d84779df1
Stephanie <syoon1@stuy.edu>
2016-06-06 01:17:03 -0400

    asdf

--- Board.java
+++ Board.java
@@ -1,6 +1,16 @@
 public class Board implements Comparable<Board> {
 	private String[][] _board;
 	private int _eval;
+	private static int[][] BOARDVALUE = {
+		{15, -7, 10, 7, 7, 10, -7, 15},
+		{-7, -10, -8, 2, 2, -8,-10, -7},
+		{10, -8, 3, 3, 3, 3, -8, 10},
+		{7, 2, 3, 1, 1, 3,  2, 7},
+		{7, 2,  3, 1, 1, 3, 2, 7},
+		{15, -7, 10, 7, 7, 10, -7, 15},
+		{-7, -10, -8, 2, 2, -8,-10, -7},
+		{10, -8, 3, 3, 3, 3, -8, 10},
+	};
 	public Board() {
 		_board = new String[8][8];
 		for (int i = 0; i < 8; i++) {
@@ -16,6 +26,9 @@ public class Board implements Comparable<Board> {
 	public String get(int i, int j) {
 		return _board[i][j];
 	}
+	public void set(int i, int j, String piece) {
+		_board[i][j] = piece;
+	}
 	public String toString() {
 		String ans = "  0 1 2 3 4 5 6 7 ";
 		ans += "\n";
@@ -28,12 +41,35 @@ public class Board implements Comparable<Board> {
 		}
 		return ans;
 	}
-	public int eval() {
+	public int getEval() {
 		return _eval;
 	}
+	public void setEval(int x) {
+		_eval = x;
+	}
+	public int heuristic() {
+		int ans = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (get(i,j).equals("x")) ans += BOARDVALUE[i][j];
+				else if (get(i,j).equals("o")) ans -= BOARDVALUE[i][j];
+			}
+		}
+		return ans;
+	}
 	public int compareTo(Board other) {
-		if (this.eval() > other.eval()) return 1;
-		else if (this.eval() == other.eval()) return 0;
+		if (this.getEval() > other.getEval()) return 1;
+		else if (this.getEval() == other.getEval()) return 0;
 		else return -1;
+	}
+	public Board inverse() {
+		Board ans = new Board();
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (this.get(i,j).equals("x")) ans.set(i,j,"o");
+				else if (this.get(i,j).equals("o")) ans.set(i,j,"x");
+			}
+		}
+		return ans;
 	}
 }
--- Game.java
+++ Game.java
@@ -15,8 +15,9 @@ public class Game {
 			System.out.flush();
 			System.out.println(_board);
 			playerTurn();
-			if (_tiles < 64) 
-				computerTurn();
+			Board inverse = _board.inverse();
+			if (_tiles < 64 && _consecPasses < 2) 
+				computerTurn(inverse);
 		}
 		showResults();
 	}
@@ -29,30 +30,68 @@ public class Game {
 		int c = scan.nextInt();
 		if (validMove(_board, r, c)) {
 			_board = makeMove(_board, r, c);
+			_tiles++;
+			_consecPasses = 0;
 		}
 		else _consecPasses++;
-	}
-	public void computerTurn() {
-		System.out.println("Computer's turn...");
-		_board = bestMove(possibleMoves(_board));
 		System.out.println(_board);
 	}
-	public static boolean validMove(Board b, int r, int c) {
+	public void computerTurn(Board inverse) {
+		System.out.println("Computer's turn...");
+		PriorityQueue<Board> pm = possibleMoves(inverse);
+		if (pm.size() != 0) {
+			_board = (bestMove(pm)).inverse();
+			_tiles++;
+			_consecPasses = 0;
+		}
+		else _consecPasses++;
+		System.out.println(_board);
+	}
+	public boolean validMove(Board b, int r, int c) {
+		
 		return true;
 	}
-	public static boolean goodFlip(Board b, int r, int c, int ud, int lr) {
-		return true;
+	public boolean goodFlip(Board b, int r, int c, int ud, int lr) {
+		
+		return false;
 	}
-	public static Board flip(Board b, int r, int c,  int ud, int lr) {
-		return null;
+	public Board flip(Board b, int r, int c,  int ud, int lr) {
+		
+		return b;
 	}
-	public static Board makeMove(Board b, int r, int c) {
-		return null;
+	public Board makeMove(Board b, int r, int c) {
+		b.set(r,c, "x");
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				if (!(b.get(r+i,c+j).equals("x"))) {
+					if (goodFlip(b, r, c, i, j)) b = flip(b, r, c, i, j);
+				}
+			}
+		}
+		return b;
 	}
-	public static PriorityQueue<Board> possibleMoves(Board b) {
-		return null;
+	public void calculate(Board b, int n) {
+		if (n == 1) b.setEval(b.heuristic());
+		else {
+			Board inverse = b.inverse();
+			PriorityQueue<Board> responses = possibleMoves(inverse);
+			calculate(bestMove(responses), n-1);
+		}
 	}
-	public static Board bestMove(PriorityQueue<Board> moves) {
+	public PriorityQueue<Board> possibleMoves(Board b) {
+		PriorityQueue<Board> moves = new PriorityQueue<Board>();
+		for (int r = 0; r < 8; r++) {
+			for (int c = 0; c < 8; c++) {
+				if (validMove(b, r, c)) {
+					Board k = makeMove(b,r,c);
+					calculate(k, _n);
+					moves.add(k);
+				}
+			}
+		}
+		return moves;
+	}
+	public Board bestMove(PriorityQueue<Board> moves) {
 		return moves.peek();
 	}
 	public void showResults() {
