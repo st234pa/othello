515c40624f9b9308c1cbc6e5387ab6130358777a
Stephanie <syoon1@stuy.edu>
2016-06-11 21:06:03 -0400

    asdf

--- Driver.java
+++ Driver.java
@@ -3,8 +3,12 @@ public class Driver {
 	public static void main(String[] args) {
 		System.out.println("OTHELLO -- by Stephanie Yoon");
 		Scanner scan = new Scanner(System.in);
-		System.out.print("n: ");
-		int n = scan.nextInt();
+		System.out.println("Enter how many moves to look ahead (1-4).");
+		int n = 5;
+		while (n<1 || n>4) {
+			System.out.print("n: ");
+			n = scan.nextInt(); 
+		}
 		System.out.println("Player is x. Computer is o.");
 		Board board = new Board();
 		Game game = new Game(board, n);
